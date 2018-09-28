945b445eb3ac15695e69bd675d78678284b3b824
Mark Mandell <markman0922@gmail.com>
2018-09-28 16:01:17 -0700

    adds responsiveness

--- project-0/scripts/app.js
+++ project-0/scripts/app.js
@@ -1,5 +1,4 @@
 
-
 $('document').ready(function() {
   $('nav a').on('click', function(e) {
     e.preventDefault();
@@ -10,4 +9,52 @@ $('document').ready(function() {
     }, 800);
   });
 
+  $('nav span').hide();
+
+
+  $(window).scroll(function() {
+    if($(window).scrollTop() >= 100) {
+      $('nav, nav a').addClass('animate');
+      $('nav span').show();
+    } else if($(window).scrollTop() <= 100) {
+      $('nav, nav a').removeClass('animate');
+      $('nav span').hide();
+    }
+  });
+
+  $(function(){
+    $('.carousel-item').eq(0).addClass('active');
+    var total = $('.carousel-item').length;
+    var current = 0;
+    $('#moveRight').on('click', function(){
+      var next = current;
+      current = current + 1;
+      setSlide(next, current);
+    });
+    $('#moveLeft').on('click', function(){
+      var prev = current;
+      current = current- 1;
+      setSlide(prev, current);
+    });
+    function setSlide(prev, next){
+      var slide = current;
+      if(next > total - 1){
+       slide = 0;
+       current = 0;
+      }
+      if(next < 0){
+        slide = total - 1;
+        current = total - 1;
+      }
+
+   $('.carousel-item').eq(prev).removeClass('active');
+   $('.carousel-item').eq(slide).addClass('active');
+    setTimeout(function(){
+
+    },800);
+
+      console.log('current '+ current);
+      console.log('prev '+ prev);
+    }
+  });
 });
